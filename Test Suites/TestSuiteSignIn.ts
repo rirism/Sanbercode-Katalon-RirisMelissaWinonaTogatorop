e19f642d96f7ed05568ea972da2ac1b2e1c0f778
rirismelissawinonatogatorop <rirismelissawinonatogatorop@localhost>
2023-10-02 22:41:46 +0700

    test suite 1

---
+++ Test Suites/TestSuiteSignIn.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteSignIn</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4948874e-b193-4d93-9470-eee2a4be9ca4</testSuiteGuid>
+</TestSuiteEntity>
